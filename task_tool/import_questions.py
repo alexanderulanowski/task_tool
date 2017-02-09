59c38d896a4773f85036fa361d6f8994d381baf0
Alexander Ulanowski <alexander.ulanowski@a1u.cc>
2017-02-10 00:54:35 +0100

    update

--- task_tool/import_questions.py
+++ task_tool/import_questions.py
@@ -5,8 +5,15 @@ from .models import Question
 
 def index(request):
     Question.objects.all().delete()
-    Question(question_number='1a', question_text='test question').save()
-    Question(question_number='1b', question_text='test question 2').save()
-    Question(question_number='1c', question_text='test question 3').save()
+    q1 = ['1a', '1b', '1c']
+    q2 = ['2a', '2b', '2c', '2d']
+    q3 = ['3a', '3b', '3c', '3d', '3e', '3f']
+    q4 = ['4a', '4b', '4c', '4d', '4e']
+
+    q = [q1, q2, q3, q4]
+
+    for q_ in q:
+        for q__ in q_:
+            Question(question_number=q__, question_text='<img src="/static/img/{}.png" /><br /><img src="/static/img/{}.png" />'.format(q__[0], q__)).save()
 
     return HttpResponse("Success")
